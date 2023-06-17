0f03d2e99514551fb9c1f3b8c9557d55bb87a5b7
Aayush Sharma <aayush.sharma.k88@gmail.com>
2023-06-18 01:53:30 +0530

    Reorganized

---
+++ ML Model/predictionModel.py
@@ -0,0 +1,9 @@
+import numpy as np
+import pandas as pd
+from sklearn.linear_model import LogisticRegression
+from sklearn.model_selection import train_test_split
+import warnings
+import pickle
+warnings.filterwarnings("ignore")
+
+data = pd.read_csv("Forest_fire.csv")
